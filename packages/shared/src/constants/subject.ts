7afb934c5b3030ff2b640e3573bbe2459e6d7d6e
junjuny0227 <junjuny0227@gmail.com>
2025-09-04 19:50:58 +0900

    feat: 일반 교과 과목 추가 및 순서 변경

--- packages/shared/src/constants/subject.ts
+++ packages/shared/src/constants/subject.ts
@@ -2,11 +2,12 @@ export const ARTS_PHYSICAL_SUBJECTS = ['체육', '음악', '미술'] as const;
 
 export const GENERAL_SUBJECTS = [
   '국어',
-  '도덕',
   '사회',
+  '도덕',
   '역사',
   '수학',
   '과학',
   '기술가정',
+  '정보',
   '영어',
 ] as const;
