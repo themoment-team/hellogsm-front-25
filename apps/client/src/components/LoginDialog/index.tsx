381da7c9febb62ed81bba7724e16d7b9a85dff23
h-0y.28 <s24015@gsm.hs.kr>
2025-09-09 09:28:49 +0900

    Revert "feat: LoginDialog PC/Mobile 구분"
    This reverts commit 40b46f7352dc0ef375b42741d93e9ea51ee9a34b.

--- apps/client/src/components/LoginDialog/index.tsx
+++ apps/client/src/components/LoginDialog/index.tsx
@@ -1,36 +1,12 @@
-import * as I from 'client/assets';
-
 import { Button, LoginButton } from 'shared/components';
 import { Dialog, DialogContent, DialogTrigger } from 'shared/components';
 import { cn } from 'shared/lib/utils';
 
-interface LoginDialogProps {
-  variant?: 'pc' | 'mobile';
-}
-
-const LoginDialog = ({ variant = 'pc' }: LoginDialogProps) => {
-  const isPC = variant === 'pc';
-
+const LoginDialog = () => {
   return (
     <Dialog>
       <DialogTrigger asChild>
-        {isPC ? (
-          <Button variant="blue">로그인</Button>
-        ) : (
-          <button
-            className={cn(
-              'flex',
-              'items-center',
-              'gap-4',
-              'text-[1.5rem]',
-              'font-bold',
-              'leading-normal',
-              'text-slate-500',
-            )}
-          >
-            <I.LoginIcon /> 로그인
-          </button>
-        )}
+        <Button variant="blue">로그인</Button>
       </DialogTrigger>
       <DialogContent className={cn('w-fit', 'p-0', '!rounded-[20px]')}>
         <div
