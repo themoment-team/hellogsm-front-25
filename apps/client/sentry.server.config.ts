ccd6b3fd559bcd738633131cfc7446729e19bb4a
seoxeon09 <s25037@gsm.hs.kr>
2026-08-02 16:07:35 +0900

    fix: Sentry tracesSampleRate 100%에서 10%로 하향 조정

--- apps/client/sentry.server.config.ts
+++ apps/client/sentry.server.config.ts
@@ -2,6 +2,6 @@ import * as Sentry from '@sentry/nextjs';
 
 Sentry.init({
   dsn: process.env.SENTRY_DSN,
-  tracesSampleRate: 1,
+  tracesSampleRate: 0.1,
   enabled: process.env.NODE_ENV === 'production' && !!process.env.SENTRY_DSN,
 });
